0c3b5f7904bf212c9d572327dacd0ac677cc4d7a
williamzhu17 <williammzhu@gmail.com>
2026-03-04 16:04:52 -0800

    wip multicore api

--- libpi/include/multicore.h
+++ libpi/include/multicore.h
@@ -2,6 +2,21 @@
  * multicore stuff
  */
 
- void multicore_entry(void);
- void start_multicore(void* entry_addr);
- 
+#define NUM_CORES 4
+
+void multicore_init(void);
+void multicore_main(unsigned core_id);
+void command_loop(unsigned core_id);
+
+unsigned multicore_get_core_id(void);
+
+// Runs a specified function on a specific core
+int multicore_call(unsigned core_id, int (*func)(void*), void* arg, int* result);
+int multicore_call_async(unsigned core_id, int (*func)(void*), void* arg);
+
+// Checks if specified core has finished running its function
+int multicore_check_done(unsigned core_id);
+
+// Waits for a specified core to finish running its function
+// Is blocking
+int multicore_wait(unsigned core_id);
--- libpi/multicore/multicore.c
+++ libpi/multicore/multicore.c
@@ -6,13 +6,62 @@
 #define CORE2_MBOX3_SET ARM_BASE + 0xAC
 #define CORE3_MBOX3_SET ARM_BASE + 0xBC
 
-void start_multicore(void* entry_addr) {
+volatile uint32_t core_online[NUM_CORES];
+volatile uint32_t cmd_funcs[NUM_CORES];
+volatile uint32_t cmd_args[NUM_CORES];
+volatile uint32_t cmd_seq[NUM_CORES];
+volatile uint32_t cmd_done[NUM_CORES];
+volatile int32_t cmd_results[NUM_CORES];
 
+// Starts multicores through multicore_entry
+void multicore_init() {
     // Write to core mailbox3 set registers
-    PUT32(CORE1_MBOX3_SET, entry_addr);
-    PUT32(CORE2_MBOX3_SET, entry_addr);
-    PUT32(CORE3_MBOX3_SET, entry_addr);
+    PUT32(CORE1_MBOX3_SET, &multicore_entry);
+    PUT32(CORE2_MBOX3_SET, &multicore_entry);
+    PUT32(CORE3_MBOX3_SET, &multicore_entry);
 
     dev_barrier();
     asm volatile("sev");
+}
+
+// Signals other cores are online and ready to receive commands
+void multicore_main(unsigned core_id) {
+    // Set core as online
+    core_online[core_id] = 1;
+    dev_barrier();
+    asm volatile("sev");
+
+    command_loop(core_id);
+}
+
+// Waits for commands from other cores
+void command_loop(unsigned core_id) {
+    while (1) {
+        // Wait for new command
+        while (cmd_seq[core_id] == cmd_done[core_id]) {
+            asm volatile("wfe");
+        }
+
+        uint32_t seq = cmd_seq[core_id];
+        uint32_t func_addr = cmd_funcs[core_id];
+        uint32_t arg = cmd_args[core_id];
+        int32_t result = 0;
+
+        // Execute function if it is not 0
+        if (func_addr != 0) {
+            int (*func)(void *) = (int (*)(void *))func_addr;
+            result = func((void *)arg);
+        }
+
+        // Mark command as done
+        cmd_results[core_id] = result;
+        dev_barrier();
+        cmd_done[core_id] = seq;
+        dev_barrier();
+        asm volatile("sev");
+    }
+}
+
+int multicore_call(unsigned core_id, int (*func)(void*), void* arg, int* result) {
+    // TODO
 }
